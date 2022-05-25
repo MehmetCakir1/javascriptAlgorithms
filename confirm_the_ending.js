faceed310b5e34542aa04996606f811d538d0f69
MehmetCakir1 <101903416+MehmetCakir1@users.noreply.github.com>
2022-05-25 22:00:50 +0300

    Add files via upload

---
+++ confirm_the_ending.js
@@ -0,0 +1,17 @@
+//Check if a string (first argument, str) ends with the given target string (second argument, target).
+//This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+//solution 1
+function confirmEnding(str, target) {
+    return str.endsWith(target);
+  }
+//solution 2
+  function confirmEnding(str, target) {
+    if(str.substr(-(target.length))==target){
+        return true
+    }else{
+        return false
+    }
+  }
+
+//confirmEnding("Bastian", "n");
---
+++ diffTwoArrays.js
@@ -0,0 +1,24 @@
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+// Note: You can return the array with its elements in any order.
+
+//solution 1
+function diffArray(arr1, arr2) {
+    return arr1.concat(arr2).filter(i => arr1.includes(i)==false|| arr2.includes(i)==false)
+}
+// console.log(diffArray([1, 2, 3, 9], [1, 2, 3, 4, 5])); 
+
+//solution 2
+function diffArray(arr1, arr2) {
+    let result=[];
+    function onlyItem(first,second){
+        for(let i=0;i<first.length;i++){
+            if(second.indexOf(first[i])==-1){
+                result.push(first[i])
+            }
+        }
+    }
+    onlyItem(arr1,arr2)
+    onlyItem(arr2,arr1)
+    return result
+    }
+// console.log(diffArray([1, 2, 3, 9], [1, 2, 3, 4, 5])); 
