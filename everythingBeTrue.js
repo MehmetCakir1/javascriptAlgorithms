a66c1f24fdf59681d385e9cee5707203a7c47127
MehmetCakir1 <101903416+MehmetCakir1@users.noreply.github.com>
2022-06-03 18:21:40 +0300

    Update everythingBeTrue.js

--- everythingBeTrue.js
+++ everythingBeTrue.js
@@ -1,5 +1,6 @@
 // Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
-// In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+// In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy.
+// Otherwise, return false.
 // In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
 // Remember, you can access object properties through either dot notation or [] notation.
 
@@ -17,5 +18,7 @@ function truthCheck(collection, pre) {
    }
   }
   
-console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: true}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));  
-console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "role"));
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, 
+                                {name: "Naomi", role: "", isBot: true}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));  
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false},
+                                {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "role"));
