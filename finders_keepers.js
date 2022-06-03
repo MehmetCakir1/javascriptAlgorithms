7920f0ebfb67bb587f0114f8f87cf534858422f9
MehmetCakir1 <101903416+MehmetCakir1@users.noreply.github.com>
2022-06-03 18:19:00 +0300

    Update finders_keepers.js

--- finders_keepers.js
+++ finders_keepers.js
@@ -1,4 +1,5 @@
-//Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+//Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. 
+//This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
 
 function findElement(arr, func) {
     let num = 0;
