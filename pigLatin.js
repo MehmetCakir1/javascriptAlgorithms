8fb4fffc84968a02dc652dc55e16a6a71b61700d
MehmetCakir1 <101903416+MehmetCakir1@users.noreply.github.com>
2022-06-03 18:25:05 +0300

    Update pigLatin.js

--- pigLatin.js
+++ pigLatin.js
@@ -1,3 +1,10 @@
+//Pig Latin is a way of altering English Words. The rules are as follows:
+//If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+//If a word begins with a vowel, just add way at the end.
+//Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
+
+
+
 function translatePigLatin(str) {
     var result = "";
     var regex = /[aeiou]/gi;
@@ -12,10 +19,10 @@ function translatePigLatin(str) {
     return result;
   }
 
-console.log(translatePigLatin("california"));
-console.log(translatePigLatin("glove"));
-console.log(translatePigLatin("rhythm"));
-console.log(translatePigLatin("schwartz"));
-console.log(translatePigLatin("algorithm"));
+// console.log(translatePigLatin("california"));
+// console.log(translatePigLatin("glove"));
+// console.log(translatePigLatin("rhythm"));
+// console.log(translatePigLatin("schwartz"));
+// console.log(translatePigLatin("algorithm"));
 
 
