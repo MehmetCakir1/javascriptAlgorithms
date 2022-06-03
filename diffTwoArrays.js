20b2e2d8f58373d25bdfb42e114cf7b52e3e7445
MehmetCakir1 <101903416+MehmetCakir1@users.noreply.github.com>
2022-06-03 18:20:44 +0300

    Update diffTwoArrays.js

--- diffTwoArrays.js
+++ diffTwoArrays.js
@@ -1,4 +1,5 @@
-// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
+//In other words, return the symmetric difference of the two arrays.
 // Note: You can return the array with its elements in any order.
 
 //solution 1
