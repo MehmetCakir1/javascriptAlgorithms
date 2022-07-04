9f531cea6e704b7f30d0e2de11d0f680cfad8570
MehmetCakir1 <101903416+MehmetCakir1@users.noreply.github.com>
2022-07-04 10:17:13 +0300

    Add files via upload

---
+++ bouncingBalls.js
@@ -0,0 +1,50 @@
+// A child is playing with a ball on the nth floor of a tall building. The height of this floor, h, is known.
+
+// He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+
+// His mother looks out of a window 1.5 meters from the ground.
+
+// How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+
+// Three conditions must be met for a valid experiment:
+// Float parameter "h" in meters must be greater than 0
+// Float parameter "bounce" must be greater than 0 and less than 1
+// Float parameter "window" must be less than h.
+// If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+
+// Note:
+// The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
+
+// Examples:
+// - h = 3, bounce = 0.66, window = 1.5, result is 3
+
+// - h = 3, bounce = 1, window = 1.5, result is -1 
+
+// (Condition 2) not fulfilled).
+
+function bouncingBall(h,  bounce,  window) {
+    let count=1;
+    if(h>0){
+        if(bounce>0 && bounce <1){
+            if(window<h){
+                while(window<h){
+                    h=h*bounce;
+                    count+=2
+                    console.log(h);
+                }
+                
+            }else{
+                return -1
+            }
+        }else{
+            return -1
+        }
+    }else{
+        return -1
+    }
+    return count-2
+}
+
+console.log(bouncingBall(3.0, 0.66, 1.5));
+console.log(bouncingBall(30.0, 0.66, 1.5));
+console.log(bouncingBall(2, 0.5, 1));
