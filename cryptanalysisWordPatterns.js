303878f970a2332a3375bbdeaf221fbb46aad140
Mehmet Çakır <101903416+MehmetCakir1@users.noreply.github.com>
2022-08-23 23:35:57 +0300

    Add files via upload

---
+++ cryptanalysisWordPatterns.js
@@ -0,0 +1,25 @@
+// In cryptanalysis, words patterns can be a useful tool in cracking simple ciphers.
+
+// A word pattern is a description of the patterns of letters occurring in a word,
+//  where each letter is given an integer code in order of appearance.
+//   So the first letter is given the code 0, and
+//    second is then assigned 1 if it is different to the first letter or 0 otherwise, and so on.
+
+// As an example, the word "hello" would become "0.1.2.2.3".
+//  For this task case-sensitivity is ignored, so "hello", "helLo" and "heLlo" will all return the same word pattern.
+
+// Your task is to return the word pattern for a given word.
+//  All words provided will be non-empty strings of alphabetic characters only, i.e. matching the regex "[a-zA-Z]+".
+
+function wordPattern(word) {
+    let result=""
+    let arr=[]
+    word.toLowerCase().split("").map((item)=>!arr.includes(item) ? arr.push(item) :null)
+        for(let k=0;k<word.length;k++){
+            result+=arr.indexOf(word[k].toLowerCase())+"."
+        }
+return result.slice(0,result.length-1)
+    }
+  console.log(wordPattern("hello"))
+  console.log(wordPattern("heLlo"))
+  console.log(wordPattern("helLo"))
